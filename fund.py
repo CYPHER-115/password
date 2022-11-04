17d8859ebaa72895c7556baf143594ca5ea8ffa4
shadow walker <116379747+CYPHER-115@users.noreply.github.com>
2022-11-04 10:25:29 +0300

    Add files via upload
    fund  is an input program that can be used to input the amount of moeny used maybe say monthly depending on your expenditure

---
+++ fund.py
@@ -0,0 +1,7 @@
+print ("welcome to maina's pc")
+print ("...where security is my middle name \n")
+
+password = input("Enter your password")
+if password == "secret":
+    print ("_**ACCESS GRANTED**_!!")
+    print ("welcome! you must be someone very important")
---
+++ password.py
@@ -0,0 +1,9 @@
+#password
+#demonstrate if statement 
+print ("welcome to maina's pc")
+print ("...where security is my middle name \n")
+
+password = input("Enter your password")
+if password == "secret":
+    print ("_**ACCESS GRANTED**_!!")
+    print ("welcome! you must be someone very important")
